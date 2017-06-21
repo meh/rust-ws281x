bcb5304850e9ae21f9b34aaf895840a87e0297a8
TheZoq2 <frans.skarman@gmail.com>
2017-06-21 21:59:47 +0200

    Update to latest version of C code to support pi zero w

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 extern crate gcc;
 
 fn main() {
-	gcc::compile_library("libws281x.a", &["c/mailbox.c", "c/ws2811.c", "c/pwm.c", "c/dma.c", "c/rpihw.c"]);
+	gcc::compile_library("libws281x.a", &["c/mailbox.c", "c/ws2811.c", "c/pwm.c", "c/dma.c", "c/rpihw.c", "c/pcm.c"]);
 }
